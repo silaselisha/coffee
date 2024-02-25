9ef754040e350d724902028d06a87235e0b505f5
Elisha <elisilas87@outlook.com>
2024-02-25 17:02:48 +0300

    ref:Change how users' server response sre sent to the client

--- pkg/handler/dto.go
+++ pkg/handler/dto.go
@@ -1,15 +1,27 @@
 package handler
 
+import (
+	"time"
+)
+
 type userLoginParams struct {
 	Email    string `bson:"email" validate:"required"`
 	Password string `bson:"password" validate:"required"`
 }
 
-type imageResultParams struct {
-	avatarName string
-	avatarFile []byte
-	err        error
+type userResponseParams struct {
+	Id          string    `json:"_id"`
+	Avatar      string    `json:"avatar"`
+	UserName    string    `json:"username"`
+	Role        string    `json:"role"`
+	Email       string    `json:"email"`
+	PhoneNumber string    `json:"phone"`
+	Verified    bool      `json:"Verified"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+type userResponseListParams []userResponseParams
 
 type passwordResetParams struct {
 	Password        string `bson:"password" validate:"required"`
